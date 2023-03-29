ac8f85bed73e38330ae75d8624feffe551edb00c
deividramirezarias <deivid.innovatec@gmail.com>
2023-03-29 17:44:53 -0500

    ajustes
    ajustes

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -21,7 +21,7 @@
     .row.mt-5(data-aos="fade-zoom")
       figure
         .video
-          iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/M_OjtRb2uE4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
 
 </template>
--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -272,7 +272,7 @@
     .row.mt-5(data-aos="zoom-in")
       figure
         .video
-          iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/7hFxPqjDZKQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     .row.my-5
       p Vea ahora la información sobre la Matriz de Evaluación de Factores Internos (MEFI), que constituye un instrumento que nos permite realizar una evaluación interna de la empresa para facilitar el análisis de la efectividad de las estrategias aplicadas, y conocer con detalle su impacto, evaluando las fortalezas y debilidades más relevantes para formular estrategias capaces de solventar, optimizar y reforzar los procesos internos ejecutados.
